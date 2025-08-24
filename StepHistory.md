3f9799c5397f129eaa2b69626244a7fb50b7adc8
reinhardherrmann <reinhardherrmann@users.noreply.github.com>
2025-08-24 07:45:25 +0200

    Step 01 Create Models, Migrations Factories and Seeders Sources: none - create all migrations - create some seeders - designed models ---

--- StepHistory.md
+++ StepHistory.md
@@ -26,8 +26,13 @@
 
 
 
-
-## Step 01 Create Models, Migrations Factories and Seeders
+## Step 02 Install Filament the individual components
+Sources: https://www.youtube.com/watch?v=YXkhgq3z7DY&t=407s
+- install components
+- install Tailwindcss V4
+- 
+---
+## Step 01 Create Models, Migrations, Factories and Seeders
 Sources: none
 - create all migrations
 - create some seeders
--- resources/views/components/layouts/app/sidebar.blade.php
+++ resources/views/components/layouts/app/sidebar.blade.php
@@ -2,6 +2,15 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="dark">
     <head>
         @include('partials.head')
+
+        <style>
+            [x-cloak] {
+                display: none !important;
+            }
+        </style>
+
+        @filamentStyles
+
     </head>
     <body class="min-h-screen bg-white dark:bg-zinc-800">
         <flux:sidebar sticky stashable class="border-e border-zinc-200 bg-zinc-50 dark:border-zinc-700 dark:bg-zinc-900">
@@ -126,6 +135,9 @@
         </flux:header>
 
         {{ $slot }}
+
+        @livewire('notifications') {{-- Only required if you wish to send flash notifications --}}
+        @filamentScripts
 
         @fluxScripts
     </body>
